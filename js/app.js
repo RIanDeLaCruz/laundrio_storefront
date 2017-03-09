f972361ec37976a09575140e96447219ecf1d379
Ian De La Cruz <ridlc10@gmail.com>
2017-03-09 16:09:08 +0800

    Add function to remove and add to routing table

--- js/app.js
+++ js/app.js
@@ -10,8 +10,62 @@ var app = (function() {
 
   var addMarkers = function (listOfCoordinates) {
     for(var i = 0; i < listOfCoordinates.length; i++) {
-      L.marker(listOfCoordinates[i]).addTo(map)
+      L.marker(listOfCoordinates[i].coordinates)
+        .addTo(map)
+        .bindPopup(
+          L.popup({"autoClose" : false})
+            .setContent(buildPopupElement(listOfCoordinates[i]))
+        )
+        .openPopup()
     }
+  }
+
+  var buildPopupElement = function (popupDetails) {
+    var customerElement = document.createElement('p')
+    customerElement.innerHTML = popupDetails.customer
+
+    var type = document.createElement('p')
+    type.innerHTML = popupDetails.type
+
+    var addBtn = document.createElement('button')
+    addBtn.innerHTML = 'Add to Queue'
+
+    var wrapper = document.createElement('div')
+    wrapper.setAttribute('id', popupDetails.id)
+    wrapper.appendChild(customerElement)
+    wrapper.appendChild(type)
+    wrapper.appendChild(addBtn)
+    addBtn.addEventListener('click', function handler(evt) {
+      appendToRoutingTable(popupDetails)
+      this.removeEventListener('click', handler)
+    })
+    return wrapper
+  }
+
+  var appendToRoutingTable = function (popupDetails) {
+    var routingTable = document.querySelector('#routing_table')
+    var tableRow = document.createElement('tr')
+    var detailsCell = document.createElement('td')
+    var actionsCell = document.createElement('td')
+    var removeBtn = document.createElement('button')
+
+    removeBtn.innerHTML = "Remove"
+    detailsCell.innerHTML = popupDetails.customer
+
+    removeBtn.addEventListener('click', function() {
+      var p = document.querySelector('#'+popupDetails.id)
+      p.addEventListener('click', function handler(evt) {
+        appendToRoutingTable(popupDetails)
+        this.removeEventListener('click', handler)
+      })
+      this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode)
+    })
+
+    tableRow.setAttribute('data-marker', popupDetails.id)
+    actionsCell.appendChild(removeBtn)
+    tableRow.appendChild(detailsCell)
+    tableRow.appendChild(actionsCell)
+    routingTable.children[1].appendChild(tableRow)
   }
 
   return {
@@ -23,9 +77,24 @@ var app = (function() {
 app.init()
 
 var coors = [
-  [14.640163, 121.072939],
-  [14.641868, 121.069343],
-  [14.639061, 121.080365]
+  {
+    "customer": "Customer Name 1",
+    "type": "Pickup",
+    "id": "customer_name",
+    "coordinates": [14.640163, 121.072939],
+  },
+  {
+    "customer": "Customer Name 2",
+    "type": "Pickup",
+    "id": "customer_name_2",
+    "coordinates": [14.641868, 121.069343],
+  },
+  {
+    "customer": "Customer Name 3",
+    "type": "Pickup",
+    "id": "customer_name_3",
+    "coordinates": [14.639061, 121.080365]
+  }
 ]
 
 app.addMarkers(coors)
